8a2ac84711aa0b9043653f979c33fb748da4a452
Md Abidur Rahman <abid19918february@gmail.com>
2026-09-01 20:31:08 +0600

    Updates have been made.

--- src/validations/review.validation.ts
+++ src/validations/review.validation.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const createReviewSchema = z.object({
   body: z.object({
-    gearItemId: z.string().uuid('Valid Gear Item UUID required'),
+    orderId: z.string().min(1, 'Order ID is required'),
     rating: z.number().min(1).max(5, 'Rating must be between 1 and 5'),
     comment: z.string().min(5, 'Comment must be at least 5 characters long'),
   }),
